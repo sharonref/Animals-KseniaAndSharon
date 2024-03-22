b4c19603375c9acf75705a72697a8a2de8ed1d12
KseniaKaplan <kseniakaplan@gmail.com>
2024-03-22 21:48:39 +0200

    signup and login updates

--- login.js
+++ login.js
@@ -1,4 +1,71 @@
+let animalsForView = JSON.parse(localStorage.getItem("animals"));
+let visitorsForView = JSON.parse(localStorage.getItem("visitors"));
+//console.log(visitorsForView);
+
+function createHTML(visitor) {
+  const newDiv = document.createElement("div");
+  newDiv.className = "visitor-card";
+  newDiv.innerHTML = `<img src="${visitor.image}" alt="${visitor.name} "/>
+  <h2>name: ${visitor.name} </h2>
+  <p>Coins: ${visitor.coins}</p>`;
+  //console.log(newDiv);
+
+  newDiv.addEventListener("click", () => {
+    loginAsVisitor();
+  });
+  return newDiv;
+}
+
+const getEmptyCardsHTMLTemplate = () => {
+  const templateWrapper = document.createElement("div");
+  templateWrapper.className = "empty-result";
+
+  const template = `
+    <h2>No Visitors Found</h2>
+    <p>We're sorry, but no vistor that matchs your search.</p>
+    <button id="clear-filter-btn" class="btn btn-dark">Clear search text</button>
+    `;
+  templateWrapper.innerHTML = template;
+  templateWrapper.children["clear-filter-btn"].addEventListener(
+    "click",
+    clearSearchBox
+  );
+  return templateWrapper;
+};
+
+const clearSearchBox = () => {
+  const input = document.getElementById("searchName");
+  input.value = "";
+  visitorsForView = [...visitors];
+  renderVisitors();
+};
+
+function renderVisitors() {
+  const visitorsCards = document.getElementById("visitors-cards");
+  visitorsCards.innerHTML = "";
+  // const visitorsForViewArray = [...visitors];
+  visitorsForView.forEach((visitor) => {
+    visitorsCards.append(createHTML(visitor));
+  });
+  if (!visitorsForView.length) {
+    visitorsCards.appendChild(getEmptyCardsHTMLTemplate());
+  }
+}
+
+const nameInput = document.getElementById("searchName");
+nameInput.addEventListener("input", () => {
+  visitorsArray = [...visitors];
+  visitorsForView = visitorsArray.filter((visitor) => {
+    return visitor.name.toLowerCase().includes(nameInput.value.toLowerCase());
+  });
+  const visitorsCards = document.getElementById("visitors-cards");
+  visitorsCards.innerHTML = "";
+  renderVisitors();
+});
+
 function loginAsVisitor(visitorName) {
   // תממשו את הלוגיקה של בחירת אורח שנכנס לגן החיות
   // שמרו את האורח שבחרתם, בלוקל סטורג' כך שבכל העמודים נדע מי האורח הנוכחי
 }
+
+window.addEventListener("load", renderVisitors);
--- signup.js
+++ signup.js
@@ -1,9 +1,13 @@
+let animalsForView = JSON.parse(localStorage.getItem("animals"));
+let visitorsForView = JSON.parse(localStorage.getItem("visitors"));
+
 function createNewVisitor(event) {
   // ביטול התנהגות דיפולטיבית של שליחת טופס
   // קראו עוד כאן: https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
   event.preventDefault();
   const nameInput = document.getElementById("visitorNewName");
-  nameInput.addEventListener("input", validateFormInputs(nameInput.value));
+  const nameString = nameInput.value;
+  console.log(nameString);
   /**
   צרו אורח חדש כאן 👇
   ניתן לפצל את הלוגיקה למספר בלתי מוגבל של פונקציות.
@@ -13,21 +17,51 @@ function createNewVisitor(event) {
 
   function validateFormInputs(value) {
     if (!value) {
+      alert("Please enter a name, dude!");
       return false;
     } else {
       return true;
     }
-    //בודק האם האינפוטים קיימים ויש בהם ערך
-    //מחזיר האם תקין או לא (בוליאני)
   }
 
   const visitorExists = (name) => {
     //מקבל שם ומחזיר תשובה האם השם האורח קיים
+    visitorsForView.forEach((element) => {
+      if (element.name === name) {
+        alert("You already exist, dude!");
+        return true;
+      } else {
+        return false;
+      }
+    });
   };
 
   const makeVisitor = (name) => {
     //מקבל שם, בודק שאין אותו כבר במערך האורחים ומחזיר אובייקט אורח
+    let newVisitor = {
+      name: name,
+      coins: 50,
+      image: "./images/Avatars1.jpeg",
+      visited: [],
+      feeded: [],
+    };
+
+    return newVisitor;
   };
+
+  if (!validateFormInputs(nameString)) {
+    return;
+  }
+  if (visitorExists(nameString)) {
+    return;
+  }
+  console.log(nameInput.value);
+  let some = makeVisitor(nameString);
+  console.log(some);
+  visitorsForView.push(makeVisitor(nameString));
+  let stringVisitores = JSON.stringify(visitorsForView);
+  localStorage.setItem("visitors", stringVisitores);
+  window.location.href = "./login.html";
 }
 
 /**************************************
